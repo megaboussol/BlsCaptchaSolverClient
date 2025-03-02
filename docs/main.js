0a38d1af7c37e00c8f48f46a2ef4c5c83d27cf0b
BOuSSOl <83629023+megaboussol@users.noreply.github.com>
2025-03-02 04:44:54 +0100

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,2 +1,20 @@
 // main.js
-import './captchaSolver.js';
+(function() {
+    'use strict';
+  
+    // Charger les dépendances
+    const scriptUrls = [
+      'https://raw.githubusercontent.com/megaboussol/BlsCaptchaSolverByMeGa/main/docs/TessereactByMeGaV6.js',
+      'https://raw.githubusercontent.com/megaboussol/BlsCaptchaSolverByMeGa/main/docs/openCvByMeGaV6.js',
+      'https://raw.githubusercontent.com/megaboussol/BlsCaptchaSolverClient/main/docs/config.js',
+      'https://raw.githubusercontent.com/megaboussol/BlsCaptchaSolverClient/main/docs/utils.js',
+      'https://raw.githubusercontent.com/megaboussol/BlsCaptchaSolverClient/main/docs/imageProcessing.js',
+      'https://raw.githubusercontent.com/megaboussol/BlsCaptchaSolverClient/main/docs/captchaSolver.js'
+    ];
+  
+    scriptUrls.forEach(url => {
+      const script = document.createElement('script');
+      script.src = url;
+      document.head.appendChild(script);
+    });
+  })();
